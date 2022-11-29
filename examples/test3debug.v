9559632f0e1de253ffd27900a880fbeeadb92964
arjunvish <arjun.viswanathan23@gmail.com>
2022-11-29 11:50:11 -0600

    Corrected debugging trace for test3

--- examples/test3debug.v
+++ examples/test3debug.v
@@ -168,7 +168,7 @@ Section Checker_SmtEx3Debug.
   (* 12. Res 1 {| 3, 1 |} *)
   Definition s13_3 := Eval vm_compute in (step_checker s12_3 (List.nth 12 (fst c3) (CTrue t_func3 t_atom3 t_form3 0))).
   Print s13_3.
-  (* s1_3 = ({| [5], [0], [4], [2] |}, 
+  (* s1_3 = ({| [5], [], [4], [2] |}, 
     0 :: nil, 4) : PArray.Map.t C.t * C.t * int *)
 
   (* If the main_checker returns true, SMTCoq has successfully managed to check the proof *)
