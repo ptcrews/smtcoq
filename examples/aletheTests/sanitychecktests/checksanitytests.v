63f9671683cb6b6d9c124bd6b6fa1b9f999357b0
arjunvish <arjun.viswanathan23@gmail.com>
2023-07-31 16:39:31 -0500

    Added debugging files

---
+++ examples/aletheTests/sanitychecktests/checksanitytests.v
@@ -0,0 +1,107 @@
+(**************************************************************************)
+(*                                                                        *)
+(*     SMTCoq                                                             *)
+(*     Copyright (C) 2011 - 2021                                          *)
+(*                                                                        *)
+(*     See file "AUTHORS" for the list of authors                         *)
+(*                                                                        *)
+(*   This file is distributed under the terms of the CeCILL-C licence     *)
+(*                                                                        *)
+(**************************************************************************)
+
+
+(* [Require Import SMTCoq.SMTCoq.] loads the SMTCoq library.
+   If you are using native-coq instead of Coq 8.9, replace it with:
+     Require Import SMTCoq.
+   *)
+Add Rec LoadPath "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/src" as SMTCoq.
+
+Require Import SMTCoq.SMTCoq.
+Require Import Bool.
+
+Require Import ZArith.
+Require Import Int31.
+
+Import BVList.BITVECTOR_LIST.
+Local Open Scope bv_scope.
+
+Import FArray.
+Local Open Scope farray_scope.
+
+(* Examples that check ZChaff certificates *)
+
+(*Local Open Scope int63_scope.*)
+Local Open Scope int31_scope.
+Local Open Scope array_scope.
+
+Section Test1VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test1.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test1.veritpf".
+End Test1VeriT.
+
+Section Test2VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test2.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test2.veritpf".
+End Test2VeriT.
+
+Section Test3VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test3.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test3.veritpf".
+End Test3VeriT.
+
+Section Test4VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test4.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test4.veritpf".
+End Test4VeriT.
+
+Section Test5VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test5.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test5.veritpf".
+End Test5VeriT.
+
+Section Test6VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test6.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test6.veritpf".
+End Test6VeriT.
+
+Section Test7VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test7.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test7.veritpf".
+End Test7VeriT.
+
+Section Test8VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test8.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test8.veritpf".
+End Test8VeriT.
+
+Section Test9VeriT.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test9.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test9.veritpf".
+End Test9VeriT.
+
+Section Test1cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test1.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test1.cvc5pf".
+End Test1cvc5.
+
+Section Test2cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test2.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test2.cvc5pf".
+End Test2cvc5.
+
+Section Test3cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test3.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test3.cvc5pf".
+End Test3cvc5.
+
+Section Test4cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test4.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test4.cvc5pf".
+End Test4cvc5.
+
+Section Test5cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test5.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test5.cvc5pf".
+End Test5cvc5.
+
+Section Test6cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test6.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test6.cvc5pf".
+End Test6cvc5.
+
+Section Test7cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test7.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test7.cvc5pf".
+End Test7cvc5.
+
+Section Test8cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test8.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test8.cvc5pf".
+End Test8cvc5.
+
+Section Test9cvc5.
+     Verit_Checker "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test9.smt2" "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/sanitychecktests/test9.cvc5pf".
+End Test9cvc5.
---
+++ examples/aletheTests/testcong/genDebug.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import os
+import shutil
+pathsmt = input('Enter full path of SMT file: ')
+pathpf = input('Enter full path of proof file: ')
+name = input('Enter file name without extension: ')
+smtname = "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/testcong/" + name + ".smt2"
+pfname = "/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/testcong/" + name + ".pf"
+#shutil.copy(pathsmt, smtname)
+#shutil.copy(pathpf, pfname)
+
+coqname = name + ".v"
+f = open(coqname, "w")
+f.write("Add Rec LoadPath \"/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/src\" as SMTCoq.\n")
+f.write("Require Import SMTCoq.SMTCoq.\n")
+f.write("Require Import Bool.\n")
+f.write("Section Benchmark.\n")
+f.write("  Verit_Checker \"" + smtname + "\" \"" + pfname + "\".\n")
+f.write("End Benchmark.\n")
+f.close()
+
+n = input('Enter a number for the variables: ')
+fname = name + "debug.v"
+f = open(fname, "w")
+f.write("Add Rec LoadPath \"/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/src\" as SMTCoq.\n")
+f.write("Require Import SMTCoq.SMTCoq.\n")
+f.write("Require Import Bool.\n")
+f.write("Require Import Int31.\n")
+f.write("Local Open Scope int31_scope.\n\n")
+f.write("Section Test" + name + "Debug.\n")
+f.write("  Parse_certif_verit t_i" + n + " t_func" + n + " t_atom" + n + " t_form" + n + " root" + n + " used_roots" + n + " trace" + n + "\n")
+f.write("  \"/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/testcong/" + name + ".smt2\"\n")
+f.write("  \"/home/arjun/Desktop/smtcoq/arjunvish-smtcoq-veritAst/smtcoq/examples/aletheTests/testcong/" + name + ".pf\".\n")
+f.write("  Definition nclauses" + n + " := Eval vm_compute in (match trace" + n + " with Certif a _ _ => a end). (* Size of the state *)\n")
+f.write("  Print nclauses" + n + ".\n")
+f.write("  Definition c" + n + " := Eval vm_compute in (match trace" + n + " with Certif _ a _ => a end). (* Certificate *)\n")
+f.write("  Print c" + n + ".\n")
+f.write("  Definition conf" + n + " := Eval vm_compute in (match trace" + n + " with Certif _ _ a => a end). (* Look here in the state for the empty clause*)\n")
+f.write("  Print conf" + n + ".\n")
+f.write("  Eval vm_compute in List.length (fst c" + n + "). (* No. of steps in certificate *)\n")
+f.write("  (* Sanity check that atoms and formulas are well-typed. Must return true *)\n")
+f.write("  Eval vm_compute in (Form.check_form t_form" + n + " && Atom.check_atom t_atom" + n + " && Atom.wt t_i" + n + " t_func" + n + " t_atom" + n + ").\n\n\n")
+
+f.write("  (* States from c" + n + " *)\n\n")
+
+f.write("  (* Start state *)\n")
+f.write("  Definition s0_" + n + " := Eval vm_compute in (add_roots (S.make nclauses" + n + ") root" + n + " used_roots" + n + ").\n")
+f.write("  Print s0_" + n + ".\n")
+f.write("  (* s0_" + n + " = ({|  |} *)\n")
+f.close()
+print("Now open file " + fname + " in Coqide, check the number of steps in the certificate.\n")
+f = open(fname, "a")
+sn = input('Enter number of steps in certificate: ')
+for i in range(int(sn)):
+  si = str(int(i) + 1)
+  i_str = str(i)
+  f.write("  Eval vm_compute in List.nth " + i_str + " (fst c" + n + ") _.\n")
+  f.write("  (* " + si + ". *)\n")
+  f.write("  Definition s" + si + "_" + n + " := Eval vm_compute in (step_checker s" + i_str + "_" + n + " (List.nth " + i_str + " (fst c" + n + ") (CTrue t_func" + n + " t_atom" + n + " t_form" + n + " 0))).\n")
+  f.write("  Print s" + si + "_" + n + ".\n")
+  f.write("  (* s" + si + "_" + n + " = ({|  |} *)\n\n")
+f.write("End Test" + n + "Debug.\n")
+f.close()
