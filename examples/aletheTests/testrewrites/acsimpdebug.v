da897317a8822a58ecb7dcf4a1d052132cfd0639
arjunvish <arjun.viswanathan23@gmail.com>
2023-08-29 11:20:31 -0500

    Minor

--- examples/aletheTests/testrewrites/acsimpdebug.v
+++ examples/aletheTests/testrewrites/acsimpdebug.v
@@ -24,17 +24,17 @@ Section TestacsimpDebug.
   (* Start state *)
   Definition s0_1 := Eval vm_compute in (add_roots (S.make nclauses1) root1 used_roots1).
   Print s0_1.
-  (* s0_1 = ({|  |} *)
+  (* s0_1 = ({| [17] |} *)
   Eval vm_compute in List.nth 0 (fst c1) _.
-  (* 1. *)
+  (* 1. ImmFlatten 1 1 12 *)
   Definition s1_1 := Eval vm_compute in (step_checker s0_1 (List.nth 0 (fst c1) (CTrue t_func1 t_atom1 t_form1 0))).
   Print s1_1.
-  (* s1_1 = ({|  |} *)
+  (* s1_1 = ({| [17] |} *)
 
   Eval vm_compute in List.nth 1 (fst c1) _.
-  (* 2. *)
+  (* 2. Res 1 {| 0, 1 |} *)
   Definition s2_1 := Eval vm_compute in (step_checker s1_1 (List.nth 1 (fst c1) (CTrue t_func1 t_atom1 t_form1 0))).
   Print s2_1.
-  (* s2_1 = ({|  |} *)
+  (* s2_1 = ({| [17] |} *)
 
 End Test1Debug.
